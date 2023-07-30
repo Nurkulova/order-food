190f174968019b0e7ae7bb151338d6deb7c3b4f1
Nurkulova <cholponnurkulova@gmail.com>
2023-07-31 02:30:04 +0300

    add Meals Summary Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Header } from "./components/header/Header";
+import { MealsSummary } from "./components/meals-summary/MealsSummary";
+
 function App() {
   return (
     <div>
        <Header/>
-      {/*<MealsSummary/>
-      <Meals/>
+      <MealsSummary/>
+      {/*<Meals/>
       <MealItem/>
        */}
     </div>
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,2 @@
-
 export {ReactComponent as BasketIcon} from './icon/basket-icon-svg.svg'
+export {default as MealsBannerImage} from './images/meals-image.png'
